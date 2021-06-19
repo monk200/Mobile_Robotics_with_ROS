66c3223806fddd083591385d0a8114648816e5c6
monk200 <monk200rs@gmail.com>
2021-06-19 11:01:24 -0500

    Create ROS package and launch file

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -5,7 +5,6 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export JACKAL_MAG_CONFIG='/home/user/catkin_ws/src/se498_lab5/jackal_simulator/jackal_pkgs/jackal_base/config/mag_config_default.yaml'
 
 # modified environment variables
 export PWD='/home/user/catkin_ws/build'
